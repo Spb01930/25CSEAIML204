350321e3346b511baebb6986d02f7093444d4fd4
SARADA PRASANNA BISWAL <saradaprasannabiswal1@gmail.com>
2026-07-21 13:15:51 +0530

    Add files via upload

---
+++ dfs_traversal.py
@@ -0,0 +1,21 @@
+def dfs(graph,node,visited):
+    
+    visited.add(node)
+    print(node,end=" ")
+       
+    for neighbour in graph[node]:
+          if neighbour not in visited:
+              dfs(graph,neighbour,visited)
+              
+graph={
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F'],
+    'D':[],
+    'E':['G'],
+    'F':[],
+    'G':[]
+}
+visited=set()
+print("DFS Traversal:")
+dfs(graph,'A',visited)
